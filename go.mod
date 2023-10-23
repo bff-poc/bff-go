fa97e745f215cacbc436e2a1a3d049bf011b934b
Axel Miguez <axel.d3v@gmail.com>
2023-10-23 19:35:46 +0200

    feat: return data to front

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module bff-go
 
 go 1.21.3
 
+require github.com/rs/cors v1.10.1
--- main.go
+++ main.go
@@ -3,24 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/rs/cors"
 	"io/ioutil"
 	"net/http"
 )
 
 type Response struct {
-	Message string `json:"message"`
+	Data string `json:"message"`
 }
 
 func main() {
 	fmt.Println("Iniciando o servidor Go...")
 
-	http.HandleFunc("/flutter-request", func(w http.ResponseWriter, r *http.Request) {
+	// Configurar as opções do CORS para permitir solicitações do seu aplicativo Flutter Web
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8081"}, // Substitua pelo endereço do seu aplicativo Flutter Web
+	})
+
+	// Use o middleware CORS nas suas rotas
+	handler := http.NewServeMux()
+	handler.HandleFunc("/flutter-request", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := http.Get("http://localhost:3000/data")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			http.Error(w, "Erro na solicitação para o backend Rails", resp.StatusCode)
+			return
+		}
 
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
@@ -39,5 +52,5 @@ func main() {
 		json.NewEncoder(w).Encode(response)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", c.Handler(handler))
 }
