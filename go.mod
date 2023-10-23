cf8310204108b84c80d4d8e56f9ad65aa24aa6ad
Axel Miguez <axel.d3v@gmail.com>
2023-10-23 12:37:28 +0200

    feat: connect bff to back and return data

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bff-go
 
-go 1.21
+go 1.21.3
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,43 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
 func main() {
+	fmt.Println("Iniciando o servidor Go...")
+
+	http.HandleFunc("/flutter-request", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://localhost:3000/data")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var response Response
+		if err := json.Unmarshal(body, &response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
